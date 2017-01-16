3d4522d39b649b09439972157232dd396b66a020
kxu <kxu@freewheel.tv>
2017-01-16 13:17:36 +0800

    add other board support

--- BYR Top 10.js
+++ BYR Top 10.js
@@ -2,7 +2,13 @@ var request = require('request'),
     cheerio = require("cheerio"),
     iconv = require('iconv-lite');
 
-var bbsurl = 'https://bbs.byr.cn/rss/topten';
+var boardName = process.argv[2];
+var bbsurl = "";
+if (boardName === "") {
+    bbsurl = "https://bbs.byr.cn/rss/topten";
+} else {
+    bbsurl = "https://bbs.byr.cn/rss/board-" + boardName;
+}
 var tmp = "<?xml version=\"1.0\"?><items>";
 
 request({
@@ -24,6 +30,9 @@ request({
             link_url = el.find("link").text(),
             author = el.find("author").text(),
             index = idx + 1;
+        if (index > 10) {
+            return false;
+        }
         tmp += "<item uid=\"top" + index + "\" arg=\"" + link_url + "\" valid=\"yes\" autocomplete=\"" + title + "\"><title>" + title + "</title><subtitle>" + author + "</subtitle><icon>icon.png</icon></item>";
     });
     tmp = tmp + "</items>";
